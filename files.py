6dbc42009b13ede969a412fea4ccab0c0b7bc42f
Stephen Stha <stephencrestha07@gmail.com>
2024-12-12 15:44:26 +0545

    thi

---
+++ files.py
@@ -0,0 +1,26 @@
+# file = open("newfile","x")
+
+# file = open("newfile","w")
+
+
+
+# Write on file
+
+# f = open("newfile","w+")
+# f.write("Hlo Love!!")
+# # print(f.read())
+
+# f.close()
+# f= open("newfile")
+# print(f.read())
+
+
+# open file
+
+# with open("newfile" ,"w") as file1:
+#     file1.write("Hello")
+# print(file1.close)
+
+
+print("Hello",end = " ")
+print("Write")
---
+++ osmodule.py
@@ -0,0 +1,8 @@
+import os
+
+
+os.chdir("c:/XAMPP/htdocs")
+
+cur_dir = os.getcwd()
+
+print(cur_dir)
